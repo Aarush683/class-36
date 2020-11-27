191f9483275d0bcec013dd8451021d4a637883a7
Aarush683 <71878811+Aarush683@users.noreply.github.com>
2020-11-27 15:08:10 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,31 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title= createElement("h2");
+        title.html("car racing game");
+        title.position(130,0)
+
+        var input = createInput("Name");
+        input.position(130,160)
+
+        var button = createButton("Press");
+        button.position(250,300);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name = input.value();
+
+            playerCount+=1
+            player.update(name);
+            player.updatecount(playerCount);
+
+            var greeting= createElement("h3");
+            greeting.html("Hello"+name);
+            greeting.position(130,160);
+        })
+    }
+}
